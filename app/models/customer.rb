f2f57aa3ea03f15d4c6406d49e849975328861ec
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-05-25 11:05:56 -0400

    Active Relationships Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,11 @@
 class Customer < ApplicationRecord
+
+    has_many :reviews
+    has_many :orders
+    has_many :menu_item, through: :reviews
+    has_many :cmenu_items, through: :order
+
+    # has_secure_password
+    # validates :username, presence: true, uniqueness: true
+    # validates :password, presence: true, length: { minimum: 6 }
 end
